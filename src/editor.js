8f47a2754e90e08c49dcbb3521bd5ec9ac965890
Jeremy Ferrell Wilson <swilson@Jeremys-Mac-mini.local>
2026-06-30 23:39:30 -0500

    Defense: opponent holds ball majority + attacks the line first, we react

--- src/editor.js
+++ src/editor.js
@@ -103,7 +103,7 @@
   }
 
   function loadExample(id) {
-    return fetch("examples/" + id + ".dbn?v=120ab6d964")
+    return fetch("examples/" + id + ".dbn?v=b65391729f")
       .then(function (r) { if (!r.ok) throw new Error("not found"); return r.text(); })
       .then(function (txt) {
         load(txt);
