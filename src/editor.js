85061e1c3c5c4dc1261191db061cafcdb4156761
storypixel <storypixel@gmail.com>
2026-06-30 19:12:47 -0500

    Cache-bust editor assets with a content-hash ?v (stamp-version.sh); no more stale-after-reload

--- src/editor.js
+++ src/editor.js
@@ -103,7 +103,7 @@
   }
 
   function loadExample(id) {
-    return fetch("examples/" + id + ".dbn")
+    return fetch("examples/" + id + ".dbn?v=09567c1e40")
       .then(function (r) { if (!r.ok) throw new Error("not found"); return r.text(); })
       .then(function (txt) {
         load(txt);
